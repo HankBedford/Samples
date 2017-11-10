fdc77a08628e757d6b04f7afe04bdf37b89dbc3b
Unknown <Zachary.Lynch1337@gmail.com>
2017-11-09 20:00:46 -0800

    added pictures and hopefully a layout

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,19 +1,40 @@
-#importing flask modules, MySQL database, and Form
+# Just playing around with flask
 
-from flask import Flask, render_template, flash, url_for, redirect, session, logging, request
+# Importing Flack modules, as well as my own data module
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+# added MySQL to macbook, and added Flask module to interact with it
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
+# always needed
 app = Flask(__name__)
+
+# call function Articles in data.py
+
+# route home screen (basically so the browser has an address to read I think
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do match')])
 
 
 @app.route('/')
 
+# call the html file
 def index():
-    return render_template('admin_page.html')
+    form = RegisterForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        return render_template('admin_page.html')
+    return render_template('admin_page.html', form=form)
+
+@app.route('/restaurant_one')
+
+def restaurant_one():
+
+    return render_template('restaurant_one.html')
 
 
 
-
-if __name__=='__main__':
+if __name__ =='__main__':
     app.run(debug=True)
